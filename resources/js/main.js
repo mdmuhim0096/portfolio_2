dbddd33083dce255a20dd3d85825b8a30f4ab7b6
mdmuhim0096 <149795726+mdmuhim0096@users.noreply.github.com>
2023-11-12 11:29:29 -0600

    Add files via upload

---
+++ resources/js/main.js
@@ -0,0 +1,15 @@
+
+
+var mixer = mixitup('.work-grid');
+
+/*navigation section*/
+
+function openMenu(){
+    document.getElementById('menu-container').style.width ="100%";
+}
+
+
+function closeMenu (){
+    document.getElementById('menu-container').style.width = "0%";
+}
+
